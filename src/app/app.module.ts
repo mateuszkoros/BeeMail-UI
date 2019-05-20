e39be06135218f588b38242934db4550f0391a3b
Mateusz Koroś <mateusz.koros@protonmail.com>
2019-05-20 20:31:38 +0200

    Removed module leftover from old template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthGuard } from './shared';
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
@@ -19,7 +18,6 @@ import { ToastrModule } from 'ngx-toastr';
         ToastrModule.forRoot()
     ],
     declarations: [AppComponent],
-    providers: [AuthGuard],
     bootstrap: [AppComponent]
 })
 export class AppModule {
