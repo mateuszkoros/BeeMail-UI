1f925e30d43d3f60a8274a134efc3fc3a6e4c2ec
Mateusz Koroś <mateusz.koros@protonmail.com>
2019-05-26 17:49:35 +0200

    Electron now runs prod version of app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const backendUrl = 'https://127.0.0.1:1944/';
+
 export const environment = {
-	production: true
+    production: true,
+    getAddressesUrl: backendUrl + 'addresses',
+    getMessagesUrl: backendUrl + 'get',
+    sendMessageUrl: backendUrl + 'send',
+    deleteMessageUrl: backendUrl + 'delete'
 };
